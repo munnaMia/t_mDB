9a07a44c28cd6dee7b47d7b4070eb69407bb0584
munnaMia <mdmunnamia987@gmail.com>
2026-05-30 17:04:30 +0600

    fix: implement initial database methods in DB struct

--- server/database/db.go
+++ server/database/db.go
@@ -1,6 +1,31 @@
-package main
+package database
 
 type DB struct {
-	
+	Storage map[string]any
 }
 
+func NewDatabase() *DB {
+	return &DB{
+		Storage: make(map[string]any),
+	}
+}
+
+func (db *DB) Write(key string, value any) {
+
+}
+
+func (db *DB) Update(key string, value any) {
+
+}
+
+func (db *DB) Delete(key string) {
+
+}
+
+func (db *DB) Get(key string) {
+
+}
+
+func (db *DB) Exists(key string) bool {
+
+}
