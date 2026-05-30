47d7f314cfa04c0175590d0ee1a601f942fe1a97
munnaMia <mdmunnamia987@gmail.com>
2026-05-30 12:36:24 +0600

    Refactor parser package: update token encoding logic and improve code structure

--- parser/parser.go
+++ parser/parser.go
@@ -1,17 +1,35 @@
 package parser
 
+import (
+	"strconv"
+)
+
 /*
-	Types:
-	* - array
-	+ - simple string
-	$ - bluk string
-	: - number
-	- - error
+Types:
+* - array
+$ - bluk string
+: - number
 */
+const (
+	Array  = "*"
+	String = "$"
+	Number = ":"
+)
+
+/*
+EOL - End Of Line
+*/
+const (
+	EOL = "\r\n"
+)
 
 // encode the tokens in tmDB serialized byte array
 func Encode(tokens []string) []byte {
-	var serializedTokens []byte
+	var serializedTokens = []byte{}
+
+	// parsed the array first
+	serializedTokens = append(serializedTokens, encode(tokens)...)
+
 	for _, token := range tokens {
 		parsedToken := encode(token)
 		serializedTokens = append(serializedTokens, parsedToken...)
@@ -20,6 +38,20 @@ func Encode(tokens []string) []byte {
 }
 
 // encode individual token as byte slices
-func encode(token string) []byte {
-	return []byte{}
+func encode(token any) []byte {
+	var prepare string
+	var serialized = []byte{}
+
+	switch v := token.(type) {
+	case []any:
+		prepare = Array + strconv.Itoa(len(v)) + EOL
+	case string:
+		prepare = String + strconv.Itoa(len(v)) + EOL + v + EOL
+	case int:
+		prepare = Number + strconv.Itoa(v) + EOL
+	}
+
+	serialized = append(serialized, []byte(prepare)...)
+
+	return serialized
 }
